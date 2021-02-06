784f1a377aa93f50c2337b8337a7a6b3e2f6659c
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2021-02-06 15:47:03 +0100

    Add setters for sprite component

--- engine/modules/rendering2d/include/ovis/rendering2d/sprite_component.hpp
+++ engine/modules/rendering2d/include/ovis/rendering2d/sprite_component.hpp
@@ -14,6 +14,10 @@ class SpriteComponent : public SceneObjectComponent {
   inline glm::vec2 size() const { return size_; }
   inline std::string texture_asset() const { return texture_asset_; }
 
+  inline void SetColor(const glm::vec4& color) { color_ = color; }
+  inline void SetSize(const glm::vec2& size) { size_ = size; }
+  inline void SetTexture(const std::string& texture_asset) { texture_asset_ = texture_asset; }
+
   json Serialize() const override;
   bool Deserialize(const json& data) override;
   const json* GetSchema() const override { return &schema; }
@@ -21,7 +25,7 @@ class SpriteComponent : public SceneObjectComponent {
  private:
   std::string texture_asset_;
   glm::vec2 size_;
-  glm::vec4 color_;
+  glm::vec4 color_ = {1.0, 1.0, 1.0, 1.0};
 
   static const json schema;
 };
