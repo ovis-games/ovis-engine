b4e9320c39932699c56dbb91a49d72f44f0e626a
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2021-03-09 00:10:07 +0100

    Add bindings and documentation for Color type

--- engine/engine/src/lua.cpp
+++ engine/engine/src/lua.cpp
@@ -1,6 +1,7 @@
 #include <middleclass.hpp>
 
 #include <ovis/math/vector.hpp>
+#include <ovis/math/color.hpp>
 #include <ovis/engine/input.hpp>
 #include <ovis/engine/lua.hpp>
 #include <ovis/engine/module.hpp>
@@ -15,6 +16,7 @@ EventHandler<void(const std::string&)> Lua::on_error;
 
 void RegisterVector2(sol::state& state);
 void RegisterVector3(sol::state& state);
+void RegisterColor(sol::state& state);
 
 void Lua::SetupEnvironment() {
   state.open_libraries(sol::lib::base, sol::lib::coroutine, sol::lib::string, sol::lib::math, sol::lib::table);
@@ -31,6 +33,7 @@ void Lua::SetupEnvironment() {
 
   RegisterVector2(state);
   RegisterVector3(state);
+  RegisterColor(state);
 
   // auto Vector3_factories = sol::factories([](sol::table table) { return Vector3(table[1], table[2], table[3]); });
   // sol::usertype<Vector3> Vector3_type = state.new_usertype<Vector3>(
@@ -648,17 +651,237 @@ void RegisterVector3(sol::state& state) {
   // @see length
   // @function __len
   // @treturn number The number of compoenents in the vector (3).
-  // @usage local v = Vector3.new()
+  // @usage local v = Vector3:new()
   // assert(#v == 3)
   vector3_type[sol::meta_function::length] = [](const Vector3& vector) { return 3; };
 
   /// Provides string conversion.
   // @function __tostring
   // @treturn string The vector components formatted as "(x, y, z)"
-  // @usage local v = Vector3.new(1, 2, 3)
+  // @usage local v = Vector3:new(1, 2, 3)
   // assert(tostring(v) == '(1.0, 2.0, 3.0)')
   vector3_type[sol::meta_function::to_string] = [](const Vector3& vector) { return fmt::format("{}", vector); };
   
+  // clang-format on
+}
+
+void RegisterColor(sol::state& state) {
+  // clang-format off
+
+  /// Represents a color usng red, green, blue and alpa.
+  // The value for r, g, b and a range from 0 to 1. E.g., the RGB values
+  // 0,0,0 respresent black and 1,1,1 respresent white. Similarly, an
+  // alpha value of 0 represents full transparency whereas an alpha value
+  // of 1 represents full opaqueness.
+  // @type Color
+  sol::usertype<Color> color_type = state.new_usertype<Color>(
+    "Color", sol::constructors<Color(float, float, float), Color(float, float, float, float)>(),
+    "new", sol::factories(
+      [](const sol::table&) { return Color(0, 0, 0, 1.0f); },
+      [](const sol::table&, float r, float g, float b) { return Color(r, g, b); },
+      [](const sol::table&, float r, float g, float b, float a) { return Color(r, g, b, a); }
+    ),
+    sol::call_constructor, sol::factories(
+      []() { return Color(0, 0, 0, 1.0f); },
+      [](float r, float g, float b) { return Color(r, g, b); },
+      [](float r, float g, float b, float a) { return Color(r, g, b, a); },
+      [](const sol::table& t) {
+        return Color(
+          t.get_or("r", t.get_or(1, 0.0f)),
+          t.get_or("g", t.get_or(2, 0.0f)),
+          t.get_or("b", t.get_or(3, 0.0f)),
+          t.get_or("a", t.get_or(4, 1.0f))
+        );
+      }
+    )
+  );
+
+  /// The red component of the vector.
+  // @field[type=number] r
+  color_type["r"] = &Color::r;
+
+  /// The green component of the vector.
+  // @field[type=number] g
+  color_type["g"] = &Color::g;
+
+  /// The blue component of the vector.
+  // @field[type=number] b
+  color_type["b"] = &Color::b;
+
+  /// The alpha component of the vector.
+  // @field[type=number] a
+  color_type["a"] = &Color::a;
+
+  /// Transparent color (0, 0, 0, 0)
+  // @field[type=Color] TRANSPARENT
+  color_type["TRANSPARENT"] = sol::property(Color::Transparent);
+
+  /// Black color (0, 0, 0, 1)
+  // @field[type=Color] BLACK
+  color_type["BLACK"] = sol::property(Color::Black);
+
+  /// White color (1, 1, 1, 1)
+  // @field[type=Color] WHITE
+  color_type["WHITE"] = sol::property(Color::White);
+
+  /// Red color (1, 0, 0, 1)
+  // @field[type=Color] RED
+  color_type["RED"] = sol::property(Color::Red);
+
+  /// Green color (0, 1, 0, 1)
+  // @field[type=Color] GREEN
+  color_type["GREEN"] = sol::property(Color::Green);
+
+  /// Blue color (0, 0, 1, 1)
+  // @field[type=Color] BLUE
+  color_type["BLUE"] = sol::property(Color::Blue);
+
+  /// Yellow color (1, 1, 0, 1)
+  // @field[type=Color] YELLOW
+  color_type["YELLOW"] = sol::property(Color::Yellow);
+
+  /// Fuchsia color (1, 0, 1, 1)
+  // @field[type=Color] FUCHSIA
+  color_type["FUCHSIA"] = sol::property(Color::Fuchsia);
+
+  /// Aqua color (0, 0, 1, 1)
+  // @field[type=Color] AQUA
+  color_type["AQUA"] = sol::property(Color::Aqua);
+
+  /// Creates a new color.
+  // The r, g and b component will be set to 0 and a will be set to 1.
+  // @function new
+  // @treturn Color
+  // @usage local c = Color:new()
+  // assert(c.r == 0 and c.g == 0 and c.b == 0 and c.a == 1)
+
+  /// Creates a new color from the arguments.
+  // @function new
+  // @param[type=number] r The value for the red channel.
+  // @param[type=number] g The value for the green channel.
+  // @param[type=number] b The value for the blue channel.
+  // @param[type=number,opt=1] a The value for the alpha channel.
+  // @treturn Color
+  // @usage local c = Color:new(1.0, 0.0, 0.5)
+  // assert(c.r == 1 and c.g == 0 and c.b == 0.5 and c.a == 1)
+
+  /// Creates a new color.
+  // Simplifies the syntax for creating colors.
+  // @function __call
+  // @treturn Color
+  // @usage -- All the following calls create the same color:
+  // local c1 = Color:new(1, 0.5, 1)
+  // local c2 = Color(1, 0.5, 1)
+  // local c3 = Color{1, 0.5, 1}
+  // local c4 = Color{r = 1, g = 0.5, b = 1}
+  // assert(c1 == c2)
+  // assert(c2 == c3)
+  // assert(c3 == c4)
+
+  /// Compares two colors for equality.
+  //
+  // Be very careful when comparing two colors as the components are floating point values!
+  //
+  // @function __eq
+  // @param[type=Color] c1
+  // @param[type=Color] c2
+  // @treturn bool
+  // @usage local c1 = Color:new(1.0, 2.0, 3.0)
+  // local c2 = Color:new(1.0, 2.0, 3.0)
+  // local v3 = Color:new(1.0, 0.0, 3.0)
+  // assert(c1 == c2) -- c1 and c2 are equal
+  // assert(c1 ~= v3) -- c1 and v3 are not
+  color_type[sol::meta_function::equal_to] = static_cast<bool (*)(const Color&, const Color&)>(ovis::operator==);
+
+  /// Adds two colors.
+  // @function __add
+  // @param[type=Color] c1
+  // @param[type=Color] c2
+  // @treturn Color
+  // @usage local c1 = Color:new(1, 0.5, 1)
+  // local c2 = Color:new(0.5, 1, 0.5)
+  // local c3 = c1 + c2
+  // assert(c3 == Color:new(1.5, 1.5, 1.5, 2))
+  color_type[sol::meta_function::addition] =
+      static_cast<Color (*)(const Color&, const Color&)>(ovis::operator+);
+
+  /// Negates a color.
+  // Negative values for colors are not necessarily useful, however,
+  // it make some algorithm more straightforward to implement.
+  // @function __unm
+  // @param[type=Color] c
+  // @treturn Color
+  // @usage local c1 = Color:new(1, -2, 3)
+  // local c2 = -c1
+  // assert(c2 == Color:new(-1, 2, -3, -1))
+  color_type[sol::meta_function::unary_minus] = static_cast<Color (*)(const Color&)>(ovis::operator-);
+
+  /// Subtracts two colors.
+  // @function __sub
+  // @param[type=Color] c1
+  // @param[type=Color] c2
+  // @treturn Color
+  // @usage local c1 = Color:new(1, 0.5, 1)
+  // local c2 = Color:new(0.5, 0.5, 0.5, 0)
+  // local v3 = c1 - c2
+  // assert(v3 == Color:new(0.5, 0, 0.5))
+  color_type[sol::meta_function::subtraction] =
+      static_cast<Color (*)(const Color&, const Color&)>(ovis::operator-);
+
+  /// Multiplies two colors or a scalar and a color.
+  // @function __mul
+  // @param[type=Color|number] c1
+  // @param[type=Color|number] c2
+  // @treturn Color
+  // @usage local c1 = Color:new(1, 0.5, 1)
+  // local c2 = Color:new(0.5, 1, 0.5)
+  // local v3 = c1 * c2 -- multiply two colors component-wise
+  // assert(v3 == Color:new(0.5, 0.5, 0.5))
+  // local v4 = c1 * 0.5 -- multiply a color and a scalar
+  // assert(v4 == Color:new(0.5, 0.25, 0.5, 0.5))
+  // local v5 = 0.5 * c1 -- you can also multiply from the other side
+  // assert(v5 == Color:new(0.5, 0.25, 0.5, 0.5))
+  color_type[sol::meta_function::multiplication] =
+      sol::overload(static_cast<Color (*)(const Color&, const Color&)>(ovis::operator*),
+                    static_cast<Color (*)(float, const Color&)>(ovis::operator*),
+                    static_cast<Color (*)(const Color&, float)>(ovis::operator*));
+
+  /// Divides two colors or a scalar and a color.
+  // @function __div
+  // @param[type=Color|number] c1
+  // @param[type=Color|number] c2
+  // @treturn Color
+  // @usage local c1 = Color:new(1, 2, 4)
+  // local c2 = Color:new(8, 4, 16)
+  // local v3 = c1 / c2 -- divides two colors component-wise
+  // assert(v3 == Color:new(0.125, 0.5, 0.25))
+  // local v4 = c1 / 2 -- divies a color by a scalar
+  // assert(v4 == Color:new(0.5, 1, 2, 0.5))
+  // -- if you divide a scalar by a color a new color is created
+  // -- by dividing the scalar by each component of the color
+  // local v5 = 2 / c1
+  // assert(v5 == Color:new(2, 1, 0.5, 2))
+  color_type[sol::meta_function::division] =
+      sol::overload(static_cast<Color (*)(const Color&, const Color&)>(ovis::operator/),
+                    static_cast<Color (*)(float, const Color&)>(ovis::operator/),
+                    static_cast<Color (*)(const Color&, float)>(ovis::operator/));
+
+  /// Provides the length operator.
+  // This returns the number of components in the color.
+  // @see length
+  // @function __len
+  // @treturn number The number of compoenents in the color (4).
+  // @usage local c = Color:new()
+  // assert(#c == 4)
+  color_type[sol::meta_function::length] = [](const Color& color) { return 4; };
+
+  /// Provides string conversion.
+  // @function __tostring
+  // @treturn string The color components formatted as "(r, g, b, a)"
+  // @usage local v = Color:new(1, 0.5, 1)
+  // assert(tostring(v) == '(1.0, 0.5, 1.0, 1.0)')
+  color_type[sol::meta_function::to_string] = [](const Color& color) { return fmt::format("{}", color); };
+
   // clang-format on
 }
 
