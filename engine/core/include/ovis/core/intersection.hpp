09ff671ce3feb6d4d2ed64156409dee75982b24a
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2021-03-31 14:17:35 +0200

    Add ray AABB intersection test

--- engine/core/include/ovis/core/intersection.hpp
+++ engine/core/include/ovis/core/intersection.hpp
@@ -36,6 +36,24 @@ struct Plane3D {
 };
 static_assert(sizeof(Plane3D) == sizeof(float) * 4);
 
+template <typename VectorType>
+struct AxisAlignedBoundingBox {
+  static_assert(is_vector<VectorType>::value, "VectorType must be Vector2, Vector3 or Vector4");
+
+  VectorType center;
+  VectorType half_extend;
+
+  static inline constexpr AxisAlignedBoundingBox<VectorType> FromCenterAndExtend(VectorType center, VectorType extend) {
+    return {center, 0.5f * extend};
+  }
+
+  static inline constexpr AxisAlignedBoundingBox<VectorType> Empty() {
+    return {VectorType::Zero(), VectorType::Zero()};
+  }
+};
+using AxisAlignedBoundingBox2D = AxisAlignedBoundingBox<Vector2>;
+using AxisAlignedBoundingBox3D = AxisAlignedBoundingBox<Vector3>;
+
 inline constexpr std::optional<Vector3> ComputeRayPlaneIntersection(Ray3D ray, Plane3D plane) {
   const Vector3 plane_normal = plane.normal();
 
@@ -47,6 +65,52 @@ inline constexpr std::optional<Vector3> ComputeRayPlaneIntersection(Ray3D ray, P
 
   const float t = Dot(plane.origin() - ray.origin, plane_normal) / denominator;
   return ray.origin + t * ray.direction;
+}
+
+struct RayAABBIntersection {
+  float t_ray_enter;
+  float t_ray_exit;
+};
+
+template <typename VectorType>
+inline constexpr std::optional<RayAABBIntersection> ComputeRayAABBIntersection(
+    Ray<VectorType> ray, AxisAlignedBoundingBox<VectorType> aabb) {
+  const VectorType inverse_direction = 1.0f / ray.direction;
+  const VectorType aabb_min = aabb.center - aabb.half_extend;
+  const VectorType aabb_max = aabb.center + aabb.half_extend;
+
+  const VectorType t0 = (aabb_min - ray.origin) * inverse_direction;
+  const VectorType t1 = (aabb_max - ray.origin) * inverse_direction;
+
+  const VectorType t_smaller = min(t0, t1);
+  const VectorType t_bigger = max(t0, t1);
+
+  const float t_min = max(0.0f, MaxComponent(t_smaller));
+  const float t_max = MinComponent(t_bigger);
+
+  return t_min <= t_max ? std::optional<RayAABBIntersection>(RayAABBIntersection{t_min, t_max})
+                        : std::optional<RayAABBIntersection>{};
+}
+
+template <typename VectorType>
+inline constexpr std::optional<RayAABBIntersection> ComputeRayAABBIntersection(Ray<VectorType> ray,
+                                                                      AxisAlignedBoundingBox<VectorType> aabb,
+                                                                      float t_min, float t_max) {
+  const VectorType inverse_direction = 1.0f / ray.direction;
+  const VectorType aabb_min = aabb.center - aabb.half_extend;
+  const VectorType aabb_max = aabb.center + aabb.half_extend;
+
+  const VectorType t0 = (aabb_min - ray.origin) * inverse_direction;
+  const VectorType t1 = (aabb_max - ray.origin) * inverse_direction;
+
+  const VectorType t_smaller = min(t0, t1);
+  const VectorType t_bigger = max(t0, t1);
+
+  t_min = max(t_min, MaxComponent(t_smaller));
+  t_max = min(t_max, MinComponent(t_bigger));
+
+  return t_min <= t_max ? std::optional<RayAABBIntersection>(RayAABBIntersection{t_min, t_max})
+                        : std::optional<RayAABBIntersection>{};
 }
 
 }  // namespace ovis
