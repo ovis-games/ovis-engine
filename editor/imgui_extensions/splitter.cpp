7b264800156ac3df24df0a9c77f3e41464c770bd
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2021-07-02 22:32:14 +0200

    Fix splitter

--- editor/imgui_extensions/splitter.cpp
+++ editor/imgui_extensions/splitter.cpp
@@ -5,7 +5,7 @@
 namespace ImGui {
 
 bool HorizontalSplitter(const char* label, float* top_height, float* bottom_height, float top_weight,
-                        float bottom_weight, float height, float top_min_height, float bottom_min_height,
+                        float bottom_weight, ImVec2 size, float top_min_height, float bottom_min_height,
                         float thickness) {
   ImGuiContext& g = *GImGui;
   ImGuiWindow* window = g.CurrentWindow;
@@ -14,25 +14,38 @@ bool HorizontalSplitter(const char* label, float* top_height, float* bottom_heig
   IM_ASSERT(top_weight > 0);
   IM_ASSERT(bottom_weight > 0);
 
-  const float content_height = height > 0.0f ? height : GetContentRegionAvail().y;
-  const float default_top_height = content_height * (top_weight / (top_weight + bottom_weight));
-  const float default_bottom_height = content_height * (bottom_weight / (top_weight + bottom_weight));
+  const auto available_content = GetContentRegionAvail();
+
+  const float content_height = size.y > 0.0f ? size.y : available_content.y - size.y;
+  const float default_top_height = content_height * (top_weight / (top_weight + bottom_weight)) - thickness * 0.5f;
+  const float default_bottom_height = content_height * (bottom_weight / (top_weight + bottom_weight)) - thickness * 0.5f;
+
+  const float content_width = size.x > 0.0f ? size.x : available_content.x - size.x;
 
   ImGuiStorage* storage = ImGui::GetStateStorage();
 
   ImGui::PushID(ImGui::GetID(label));
   auto top_window_height_id = ImGui::GetID("TopWindowSize");
   auto bottom_window_height_id = ImGui::GetID("BottomWindowSize");
+  auto content_height_id = ImGui::GetID("ContentHeight");
   ImGui::PopID();
 
   *top_height = storage->GetFloat(top_window_height_id, default_top_height);
   *bottom_height = storage->GetFloat(bottom_window_height_id, default_bottom_height);
+  const float previous_content_height = storage->GetFloat(content_height_id, content_height);
+  storage->SetFloat(content_height_id, content_height);
+
+  if (previous_content_height != content_height) {
+    const float factor = content_height / previous_content_height;
+    *top_height = std::round(*top_height * factor); // TODO: is the round necessary? are non-integer values okay for sizes?
+    *bottom_height = content_height - thickness - *top_height;
+  }
 
   ImRect bb;
   bb.Min.x = window->DC.CursorPos.x;
   bb.Min.y = window->DC.CursorPos.y + *top_height;
 
-  const auto item_size = CalcItemSize(ImVec2(height, thickness), 0.0f, 0.0f);
+  const auto item_size = CalcItemSize(ImVec2(content_width, thickness), 0.0f, 0.0f);
   bb.Max.x = bb.Min.x + item_size.x;
   bb.Max.y = bb.Min.y + item_size.y;
 
--- editor/imgui_extensions/splitter.hpp
+++ editor/imgui_extensions/splitter.hpp
@@ -3,7 +3,7 @@
 namespace ImGui {
 
 bool HorizontalSplitter(const char* label, float* top_height, float* bottom_height, float top_weight = 1.0f,
-                        float bottom_weight = 1.0f, float height = 0.0f, float top_min_height = 4.0f, float bottom_min_height = 4.0f,
+                        float bottom_weight = 1.0f, ImVec2 size = ImVec2(0.0f, 0.0f), float top_min_height = 4.0f, float bottom_min_height = 4.0f,
                         float thickness = 4.0f);
 
 }
