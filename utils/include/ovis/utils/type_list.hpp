79f1f732fd43b5af1000b67939e0cbb4416c8aab
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-04-24 23:48:50 +0200

    Add size value to TypeList

--- utils/include/ovis/utils/type_list.hpp
+++ utils/include/ovis/utils/type_list.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <cstdint>
+
 namespace ovis {
 
-template <typename... Ts> struct TypeList {};
+template <typename... Ts> struct TypeList {
+  static constexpr std::size_t size = sizeof...(Ts);
+};
 
 }  // namespace ovis
