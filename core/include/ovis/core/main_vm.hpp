ee7ccb076e10208bb5ad82bb73657a371e67adb1
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-06-09 23:32:28 +0200

    Enable game module loading

--- core/include/ovis/core/main_vm.hpp
+++ core/include/ovis/core/main_vm.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "ovis/utils/result.hpp"
+#include "ovis/vm/parse_script_error.hpp"
+#include <map>
+#include <vector>
+
 namespace ovis {
 
 class VirtualMachine;
@@ -7,6 +12,12 @@ class VirtualMachine;
 extern VirtualMachine* main_vm;
 
 void InitializeMainVM();
+
+struct LoadModuleError {
+  std::map<std::string, std::vector<ParseScriptError>> script_errors;
+};
+
+Result<LoadModuleError> LoadGameModule();
 
 }  // namespace ovis
 
--- core/src/main_vm.cpp
+++ core/src/main_vm.cpp
@@ -1,6 +1,9 @@
-#include <ovis/vm/virtual_machine.hpp>
-#include <ovis/core/main_vm.hpp>
-#include <ovis/core/vm_bindings.hpp>
+#include "ovis/core/main_vm.hpp"
+
+#include "ovis/core/asset_library.hpp"
+#include "ovis/core/vm_bindings.hpp"
+#include "ovis/vm/script_parser.hpp"
+#include "ovis/vm/virtual_machine.hpp"
 
 namespace ovis {
 VirtualMachine* main_vm;
@@ -16,6 +19,27 @@ void InitializeMainVM() {
     assert(module);
     binding.register_function(module.get());
   }
+}
+
+Result<LoadModuleError> LoadGameModule() {
+  assert(main_vm);
+
+  if (main_vm->GetModule("Game")) {
+    main_vm->DeregisterModule("Game");
+  }
+
+  const auto game_module = main_vm->RegisterModule("Game");
+  LoadModuleError error;
+  for (const auto& asset_id : GetApplicationAssetLibrary()->GetAssetsWithType("script")) {
+    const auto script_asset_content = GetApplicationAssetLibrary()->LoadAssetTextFile(asset_id, "script");
+    assert(script_asset_content);
+
+    const auto parse_result = ParseScript(main_vm, json::parse(*script_asset_content));
+    if (!parse_result) {
+      error.script_errors[asset_id] = parse_result.error();
+    }
+  }
+  
 }
 
 }  // namespace ovis
