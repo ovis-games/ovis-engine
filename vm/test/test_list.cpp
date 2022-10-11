e0c50fe0fbf4174aace86bc7edaa3cdf6a91e880
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-10-11 13:37:16 +0200

    Add tests for non trivial type: string

--- vm/test/test_list.cpp
+++ vm/test/test_list.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 
 #include "catch2/catch.hpp"
+#include <string>
 
 #include "ovis/utils/log.hpp"
 #include "ovis/vm/list.hpp"
 
 using namespace ovis;
 
-TEST_CASE("Construct list", "[ovis][vm][List]") {
+TEST_CASE("List of numbers", "[ovis][vm][List]") {
   VirtualMachine vm;
   List number_list(vm.GetType<double>());
 
@@ -38,6 +39,36 @@ TEST_CASE("Construct list", "[ovis][vm][List]") {
   for (List::SizeType i = 0; i < 50; ++i) {
     REQUIRE(number_list.Get<double>(i) == 100 + i);
   }
+}
 
-  number_list.Resize(250);
+TEST_CASE("List of strings", "[ovis][vm][List]") {
+  VirtualMachine vm;
+  List string_list(vm.GetType<std::string>());
+
+  REQUIRE(string_list.size() == 0);
+  REQUIRE(string_list.capacity() == 0);
+
+  string_list.Reserve(100);
+
+  REQUIRE(string_list.size() == 0);
+  REQUIRE(string_list.capacity() == 100);
+
+  string_list.Resize(50);
+
+  REQUIRE(string_list.size() == 50);
+  REQUIRE(string_list.capacity() == 100);
+
+  for (List::SizeType i = 0; i < 50; ++i) {
+    REQUIRE(string_list.Get<std::string>(i).empty());
+    string_list.Set<std::string>(i, std::to_string(100 + i));
+    REQUIRE(string_list.Get<std::string>(i) == std::to_string(100 + i));
+  }
+
+  string_list.Reserve(200);
+
+  REQUIRE(string_list.size() == 50);
+  REQUIRE(string_list.capacity() == 200);
+  for (List::SizeType i = 0; i < 50; ++i) {
+    REQUIRE(string_list.Get<std::string>(i) == std::to_string(100 + i));
+  }
 }
