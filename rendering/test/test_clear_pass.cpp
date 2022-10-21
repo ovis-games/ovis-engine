9b6176c1448026eba9d11af6000e8f5ba46f22bc
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-10-21 14:37:33 +0200

    Add utility functions for testing rendering

--- rendering/test/test_clear_pass.cpp
+++ rendering/test/test_clear_pass.cpp
@@ -13,26 +13,14 @@
 using namespace ovis;
 
 TEST_CASE("Test clear pass", "[ovis][rendering][ClearPass]") {
-  auto window = SDL_CreateWindow("ClearPass", 100, 100, 100, 100, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN);
-  auto gl_context = SDL_GL_CreateContext(window);
-  SDL_GL_MakeCurrent(window, gl_context);
-
-  int width, height;
-  SDL_GetWindowSize(window, &width, &height);
-  GraphicsContext context(Vector2(width, height));
-
+  test::TestWindow window;
   Scene scene;
-  scene.frame_scheduler().AddJob<ClearPass>(&context, Color::Aqua());
+  scene.frame_scheduler().AddJob<ClearPass>(&window.graphics_context, Color::Aqua());
   scene.Prepare();
   scene.Play();
   scene.Update(0.0);
 
-  std::vector<uint32_t> framebuffer(100 * 100);
-  glReadPixels(0, 0, 100, 100, GL_RGBA,  GL_UNSIGNED_BYTE, framebuffer.data());
-
-  for (int y = 0; y < 100; ++y) {
-    for (int x = 0; x < 100; ++ x) {
-      REQUIRE(framebuffer[y * 100 + x] == ConvertToRGBA8(Color::Aqua()));
-    }
+  for (const auto pixel : window.GetFramebufferContent()) {
+    REQUIRE(pixel == ConvertToRGBA8(Color::Aqua()));
   }
 }
--- utils/test_utils/ovis/test/utils.hpp
+++ utils/test_utils/ovis/test/utils.hpp
@@ -16,3 +16,64 @@
 
 // Define the namespace to make the statement 'using namespace ovis' not an error in snippets.
 namespace ovis {}
+
+#if __has_include("ovis/graphics/graphics_context.hpp")
+#include "SDL_video.h"
+#include "ovis/graphics/graphics_context.hpp"
+
+// Define the namespace to make the statement 'using namespace ovis' not an error in snippets.
+namespace ovis {
+namespace test {
+
+namespace detail {
+
+SDL_GLContext CreateOpenGLContext(SDL_Window* window) {
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+#if !defined(__IPHONEOS__) && !defined(__EMSCRIPTEN__)
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+#endif
+
+  auto context = SDL_GL_CreateContext(window);
+  SDL_assert(context != nullptr);
+  SDL_GL_MakeCurrent(window, context);
+  LogI("OpenGL version: {}", glGetString(GL_VERSION));
+
+  return context;
+}
+
+}  // namespace
+
+struct TestWindow {
+  std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window;
+  std::unique_ptr<void, decltype(&SDL_GL_DeleteContext)> gl_context;
+  GraphicsContext graphics_context;
+  int client_width;
+  int client_height;
+
+  TestWindow(int width = 1280, int height = 720, bool show = false)
+      : window(SDL_CreateWindow(Catch::getResultCapture().getCurrentTestName().c_str(), 0, 0, width, height,
+                                show ? SDL_WINDOW_OPENGL : SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN),
+               SDL_DestroyWindow),
+        gl_context(detail::CreateOpenGLContext(window.get()), SDL_GL_DeleteContext),
+        graphics_context(Vector2(1280, 720)) {
+    SDL_GetWindowSize(window.get(), &client_width, &client_height);
+    graphics_context.SetFramebufferSize(client_width, client_height);
+  }
+
+  std::vector<uint32_t> GetFramebufferContent() {
+    std::vector<uint32_t> framebuffer(client_width * client_height);
+    glReadPixels(0, 0, client_width, client_height, GL_RGBA,  GL_UNSIGNED_BYTE, framebuffer.data());
+    return framebuffer;
+  }
+};
+
+}  // namespace test
+}  // namespace ovis
+
+#endif
